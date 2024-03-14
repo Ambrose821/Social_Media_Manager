270ac0c7f9720f27db3d5b484141c2b4ff61105e
Ambrose821 <mclaughlinambrose@gmail.com>
2024-03-13 23:48:22 -0400

    Check if REEL Container Status is ready

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const sixty_graph_key = require('./config/insta_access')
 // use settimeout to notify me when the key is gonna expire
 
 
-const {page_connect, get_creation_id, post_insta_photo} = require('./utils/insta_assist')
+const {page_connect, get_creation_id, post_insta_photo ,get_insta_creation_id_status, uploadReelsToContainer, getStatusOfUploadContainer } = require('./utils/insta_assist')
 
 //If you need to get the instagram id for env variables look down
 //page_connect(process.env.SHUFFLE_MEDIA_FACEBOOK_ID)
@@ -23,7 +23,30 @@ const {page_connect, get_creation_id, post_insta_photo} = require('./utils/insta
 //get_creation_id(process.env.SHUFFLE_MEDIA_INSTAGRAM_ID, 'https://politicsnigeria.com/wp-content/uploads/2024/02/philip-shaibu.jpg','Edo deputy gov raises alarm, says there are plans to impeach him over controversial PDP primary')
 
 //drumroll please...
-post_insta_photo(process.env.SHUFFLE_MEDIA_INSTAGRAM_ID, 'https://politicsnigeria.com/wp-content/uploads/2024/02/philip-shaibu.jpg','Edo deputy gov raises alarm, says there are plans to impeach him over controversial PDP primary')
+
+const tester = async() =>{
+var creation_id = await post_insta_photo(process.env.SHUFFLE_MEDIA_INSTAGRAM_ID, 'https://img-9gag-fun.9cache.com/photo/aBdq5X1_460sv.mp4','Hello World', "reel")
+
+
+var status = getStatusOfUploadContainer(process.env.CURRENT_LONG_TOKEN,  creation_id)
+var counter =1
+while(status != "FINISHED"){
+    status =  await getStatusOfUploadContainer(process.env.CURRENT_LONG_TOKEN, creation_id);
+
+    console.log("Checked"+  ++counter)
+}
+console.log("ready")
+}
+
+// /post_insta_photo(process.env.SHUFFLE_MEDIA_INSTAGRAM_ID, 'https://politicsnigeria.com/wp-content/uploads/2024/02/philip-shaibu.jpg','Hello World',)
+//
+tester()
+
+//var creation_id =  post_insta_photo(process.env.SHUFFLE_MEDIA_INSTAGRAM_ID, 'https://img-9gag-fun.9cache.com/photo/aBdq5X1_460sv.mp4','Hello World', "reel")
+
+
+var status = getStatusOfUploadContainer(process.env.CURRENT_LONG_TOKEN, '18023004295891477')
+//get_insta_creation_id_status(creation_id)
 const app = new express()
 
 app.use(express.urlencoded({extended:false}))
--- utils/insta_assist.js
+++ utils/insta_assist.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { response } = require('express');
 
 //takes a facebook page id and returns the instagram id that is linked to the page
 const page_connect =  async(page_id) =>{
@@ -21,7 +22,7 @@ const get_creation_id = async (insta_id, media_url,caption,content_type) =>{
         var url =`https://graph.facebook.com/v19.0/${insta_id}/media?media_type=REELS&video_url=${media_url}&caption=${caption}&access_token=${process.env.CURRENT_LONG_TOKEN}`;
     }
     else{
-        var url =`https://graph.facebook.com/v19.0/${insta_id}/media?image_url=${img_url}&caption=${caption}&access_token=${process.env.CURRENT_LONG_TOKEN}`
+        var url =`https://graph.facebook.com/v19.0/${insta_id}/media?image_url=${media_url}&caption=${caption}&access_token=${process.env.CURRENT_LONG_TOKEN}`
     }
 
     const response = await axios.post(url)
@@ -33,18 +34,63 @@ const get_creation_id = async (insta_id, media_url,caption,content_type) =>{
 
 }   
 
+const get_insta_creation_id_status = async (creation_id) => {
+    try{
+        const url = `https://graph.facebook.com/v19.0/${creation_id}?fields=status_code,status&access_token=${process.env.CURRENT_LONG_TOKEN}`
+
+        const response = await axios.get(url);
+       // console.log(response.data)}
+        
+}catch(err){
+    console.log(err)
+}
+
+}
+
+
+const getStatusOfUploadContainer = async (accessToken, igContainerId) => {
+    const response = await axios.get(
+      `https://graph.facebook.com/v19.0/${igContainerId}`,
+      { params: { access_token: accessToken, fields: 'status_code' } }
+    );
+        console.log(response.data.status_code)
+    return response.data.status_code;
+  };
+
+
 
 //This is seperate from the creation_id function because i believe i'll need a different procedure for different types of posts EX)videos, reels, stories, etc
 const post_insta_photo = async(insta_id, media_url,caption,content_type) =>{
-    const creation_id = await get_creation_id(insta_id, media_url_url,caption,content_type)
-
-    const url = `https://graph.facebook.com/v19.0/${insta_id}/media_publish?creation_id=${creation_id}&access_token=${process.env.CURRENT_LONG_TOKEN}`
+    const creation_id = await get_creation_id(insta_id, media_url,caption,content_type)
+    console.log(creation_id);
+    return creation_id
+     const url = `https://graph.facebook.com/v19.0/${insta_id}/media_publish?creation_id=${creation_id}&access_token=${process.env.CURRENT_LONG_TOKEN}`
     const response = await axios.post(url);
     console.log(response.data)
 
 }
 
 
+const uploadReelsToContainer = async (
+    accessToken,
+    instagramAccountId,
+    caption,
+    videoUrl,
+    coverUrl
+  ) => {
+    const response = await axios.post(
+      `https://graph.facebook.com/v19.0/${instagramAccountId}/media`,
+      {
+        access_token: accessToken,
+        caption,
+        media_type: 'REELS',
+        video_url: videoUrl,
+        cover_url: coverUrl,
+      }
+    );
+    console.log
+    return response.data;
+  };
 //url: https://politicsnigeria.com/wp-content/uploads/2024/02/philip-shaibu.jpg
 //caption "Edo deputy gov raises alarm, says there are plans to impeach him over controversial PDP primary"
-module.exports = {page_connect, get_creation_id, post_insta_photo};
+module.exports = {page_connect, get_creation_id, post_insta_photo,get_insta_creation_id_status, uploadReelsToContainer, getStatusOfUploadContainer };
